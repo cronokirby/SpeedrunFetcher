52a9cf749d632deb1965aad47c249074996c9c62
cronokirby <cronokirby@gmail.com>
2016-12-28 17:43:03 +0100

    Finally have something working

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import Control.Monad
+
+import Api
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+    putStrLn "Enter a command: use --help for a list of commands"
+    forever $ do
+        command <- getLine
+        handleCommand (words command)
+
+handleCommand :: [String] -> IO ()
+handleCommand (command:xs)
+    | command == "--help" = putStrLn
+        "Use '--categories gamename' to get a list of categories for a game"
+handleCommand (command:arg:xs)
+    | command == "--categories" = categories arg
+
+type GameName = String
+categories :: GameName -> IO ()
+categories abbreviation = do
+    let url = "http://www.speedrun.com/api/v1/games/"
+            ++ abbreviation ++ "/categories"
+    putStrLn url
+    categories <- fetchCategories url
+    case categories of
+        Left err -> putStrLn err
+        Right cats -> putStrLn $ "Here's a list of categories for "
+                            ++ abbreviation ++ ":\n" ++ show cats
--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Parsing
-    ( CategoryData
-    , Category)
-    where
+module Parsing (parseCategories) where
 
 import Data.Aeson
+import qualified Data.ByteString.Lazy as B
 
-
-data CategoryData = CategoryData { cData :: !Array }
+data CategoryData = CategoryData { cats :: [Category] } deriving (Show)
 instance FromJSON CategoryData where
     parseJSON (Object o) = CategoryData <$> (o .: "data")
 
 
-data Category = Category { name :: String}
+data Category = Category { name :: String} deriving (Show)
 instance FromJSON Category where
     parseJSON (Object o) = Category <$> (o .: "name")
+
+parseCategories :: IO B.ByteString -> IO (Either String [String])
+parseCategories json = do
+    catData <- (eitherDecode <$> json) :: IO (Either String CategoryData)
+    return $ catData >>= \catData -> return $ map name $ cats catData
