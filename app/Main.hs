d79c146280051d2d7e8051fdfb751a1429c35cbe
cronokirby <cronokirby@gmail.com>
2016-12-29 15:11:25 +0100

    Added searching for abbreviation in command calls; everything works!

--- app/Main.hs
+++ app/Main.hs
@@ -13,16 +13,23 @@ main = do
 
 handleCommand :: [String] -> IO ()
 handleCommand (command:xs)
-    | command == "--help" = putStrLn $
-        "Use '--categories gamename' to get a list of categories for a game\n"
-        ++ "Use '--search GameName' to find the abbreviation for a game"
+    | command == "--help" = putStrLn
+        "Use '--categories gamename' to get a list of categories for a game\n\
+        \Use '--search GameName' to find the abbreviation for a game\n\
+        \Use '--wr gamename' to get a wr for a game\n\
+        \Use '--time place gamename' to get the nth place time\n\
+        \These commands require the game's abbreviation, to use them \
+        \with a full name, use ?command with the game's name, with \
+        \dashes in place of spaces"
 handleCommand (command:arg:xs)
     | command == "--categories" = categories arg
+    | command == "?categories"  = abbreviated categories arg
     | command == "--search"     = search arg
 handleCommand (command:game:xs)
-    | command == "--wr" = getTime 1 game (unwords xs)
+    | command == "--wr" = getTime 1 (unwords xs) game
+    | command == "?wr"  = abbreviated (getTime 1 (unwords xs)) game
 handleCommand (command:place:game:xs)
-    | command == "--time" = getTime (read place) game (unwords xs)
+    | command == "--time" = getTime (read place) (unwords xs) game
 handleCommand _ = putStrLn "unkown command"
 
 
@@ -38,7 +45,8 @@ categories abbreviation = do
                             ++ abbreviation ++ ":\n" ++ show cats
 
 search :: GameName -> IO ()
-search gameName = do
+search dashedName = do
+    let gameName = map (\c -> if c == '-' then ' ' else c) dashedName
     let url = "http://www.speedrun.com/api/v1/games?name=" ++ gameName
     abbreviation <- fetchAbbreviation url
     case abbreviation of
@@ -47,8 +55,8 @@ search gameName = do
                               ++ "' is:\n" ++ abbr
 
 
-getTime :: Int -> GameName -> String -> IO ()
-getTime place abbreviation catName = do
+getTime :: Int -> String -> GameName -> IO ()
+getTime place catName abbreviation  = do
     let catUrl = "http://www.speedrun.com/api/v1/games/"
             ++ abbreviation ++ "/categories"
     leaderboard <- fetchLeaderboard catName catUrl
@@ -58,4 +66,14 @@ getTime place abbreviation catName = do
             runString <- fetchTime (place-1) url
             case runString of
                 Left err -> putStrLn err
-                Right info -> putStrLn info
+                Right runString -> putStrLn runString
+
+--Fetches the abbreviation for a game, before calling a function with that
+abbreviated :: (GameName -> IO ()) -> String -> IO ()
+abbreviated func dashedName = do
+    let gameName = map (\c -> if c == '-' then ' ' else c) dashedName
+    let url = "http://www.speedrun.com/api/v1/games?name=" ++ gameName
+    abbreviation <- fetchAbbreviation url
+    case abbreviation of
+        Left err -> putStrLn err
+        Right abbreviation -> func abbreviation
