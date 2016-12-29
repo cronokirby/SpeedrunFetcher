e7ee018cabcf2fca86a38f8ad93fa59478b675ad
cronokirby <cronokirby@gmail.com>
2016-12-29 01:29:41 +0100

    Started working on run fetching, not functional yet...

--- src/Api.hs
+++ src/Api.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Api ( fetchCategories, fetchAbbreviation ) where
 
-import Network.HTTP.Conduit (simpleHttp)
+import Data.Maybe
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
+import Network.HTTP.Conduit (simpleHttp)
 
 import Parsing
 
@@ -32,3 +33,18 @@ fetchLeaderboard :: String -> Url -> IO (Either String String)
 fetchLeaderboard catName url = do
     let json = fetchJSON url
     parseLeaderboard catName json
+
+-- needs IO for fetching a name, converts a Run into a tuple of info
+type Rank = String  -- I.E. WR, 1st place time
+runToString :: IO (Either String Run) -> Rank -> IO (Either String String)
+runToString parsedRun rank= do
+    run <- parsedRun
+    return $ case run of
+        Left err -> Left err
+        Right runInfo -> Right $ process runInfo
+        where
+          process :: Run -> String
+          process run = "The " ++ rank ++ "is " ++ timeString ++ " by " ++ name
+              where
+                name = fromMaybe "fetchName $ userLink run" $ playerName run
+                timeString = "formatTime $ time run"
--- src/Parsing.hs
+++ src/Parsing.hs
@@ -2,13 +2,21 @@
 module Parsing ( parseCategories
                , Game (..)
                , parseGameWith
-               , parseLeaderboard) where
+               , parseLeaderboard
+               , Run (..)
+               , parseRun) where
 
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
+-- Just imported for resolvename, which has to be here cause of Circular depends
+import Network.HTTP.Conduit (simpleHttp)
+
 
 type Json = IO ByteString
 type Url = String
+
+fetchJSON :: Url -> Json
+fetchJSON = simpleHttp
 
 data CategoryData = CategoryData { cats :: [Category] } deriving (Show)
 instance FromJSON CategoryData where
@@ -20,7 +28,7 @@ data Category = Category { catName :: String
 instance FromJSON Category where
     parseJSON (Object o) = Category
         <$> (o .: "name")
-        <*> ((last <$> o .: "links") >>= (.: "uri"))
+        <*> (last <$> o .: "links" >>= (.: "uri"))
 
 
 parseCategories :: Json -> IO (Either String [String])
@@ -51,3 +59,26 @@ parseGameWith :: (Game -> String) -> Json -> IO (Either String String)
 parseGameWith info json = do
     gameData <- (eitherDecode <$> json) :: IO (Either String Game)
     return $ gameData >>= \gameData -> return $ info gameData
+
+
+data RunData = RunData { runs :: [Run]} deriving (Show)
+instance FromJSON RunData where
+    parseJSON (Object o) = RunData
+        <$> ((o .: "data") >>= (.: "runs"))
+
+type Seconds = Int
+data Run = Run { playerName :: Maybe String, userLink :: Url
+               , time :: Seconds, video :: Url} deriving (Show)
+instance FromJSON Run where
+    parseJSON (Object o) = Run
+        <$> (player >>= (.:? "name"))
+        <*> (player >>= (.: "uri"))
+        <*> ((o .: "times") >>= (.: "primary_t"))
+        <*> ((head <$> (o .: "videos") >>= (.: "links")) >>= (.: "uri"))
+        where
+          player = head <$> o .: "players"
+
+parseRun :: Int -> Json -> IO (Either String Run)
+parseRun place json = do
+    runData <- (eitherDecode <$> json) :: IO (Either String RunData)
+    return $ runData >>= \runData -> return $ runs runData !! place
